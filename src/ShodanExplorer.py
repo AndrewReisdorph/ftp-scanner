cc34787507822c2a2584da2377179eaaf7931d0c
unknown <dmeltz014@gmail.com>
2018-06-25 21:34:03 -0600

    Adding CSRF token to fix login

--- src/ShodanExplorer.py
+++ src/ShodanExplorer.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import html
+from lxml import etree
 
 USER_AGENT = r'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/' \
              r'537.36'
@@ -11,20 +12,24 @@ class ShodanExplorer(object):
         self.session = requests.session()
         self.search_url = r'https://www.shodan.io/search'
         self.header = {'User-Agent': USER_AGENT}
+        self.csrf = None
         self.init_cookies()
         self.org_list = ['Comcast Cable', 'AT&T', 'Time Warner Cable', 'CenturyLink', 'Charter', 'Verizon', 'Cox',
                          'Optimum', 'Frontier', 'Suddenlink', 'Earthlink', 'Windstream', 'Cable One', 'NetZero', 'Juno',
                          'AOL', 'MSN', 'Mediacom']
 
     def init_cookies(self):
-        self.session.get(r'https://account.shodan.io/login')
+        resp = self.session.get(r'https://account.shodan.io/login')
+        tree = etree.HTML(resp.content)
+        self.csrf = tree.xpath('//input[@name="csrf_token"]/@value')[0]
 
     def login(self):
         data = {'username': 'meltztest',
                 'password': 'shodantest',
                 'grant_type': 'password',
                 'continue': r'https://account.shodan.io',
-                'login_submit': 'Log in'}
+                'login_submit': 'Log in',
+                'csrf_token': self.csrf}
 
         response = self.session.post(r'https://account.shodan.io/login', data=data, headers=self.header)
 
@@ -52,7 +57,7 @@ class ShodanExplorer(object):
 if __name__ == '__main__':
     e = ShodanExplorer()
     login_status = e.login()
-    print "Login: {}".format(login_status)
+    print( "Login: {}".format(login_status) )
 
     if login_status:
-        print e.get_query_results(r'ftp country:"US" port:"21" org:"Comcast Cable"')
+        print( e.get_query_results(r'ftp country:"US" port:"21" org:"Comcast Cable"') )
